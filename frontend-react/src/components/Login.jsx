96df47c6df7f78512e45ded1af58f336277c7f6a
Dipal <katuwaldipal05@gmail.com>
2025-11-05 20:57:18 +0545

    registration page

---
+++ frontend-react/src/components/Login.jsx
@@ -0,0 +1,6 @@
+const Login = () => {
+  return (
+    <div className='text-light container'>Login</div>
+  )
+}
+export default Login
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,7 +1,9 @@
 import Button from './Button'
+
 const Main = () => {
   return (
     <>
+    
     <div className='container'>
     <div className='p-5 text-center bg-light-dark rounded'>
       <h1 className='text-light'>Nepse Prediction Portal</h1>
@@ -10,6 +12,7 @@ const Main = () => {
     </div>
 
     </div>
+    
     </>
   )
 
