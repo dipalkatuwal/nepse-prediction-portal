5d42f1b896c6f381227c98a8e50ea41120c6cd2e
Dipal <katuwaldipal05@gmail.com>
2025-11-05 12:25:00 +0545

    homepage-react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer-py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; 2025 - Buit with ❤️ by Dipal Katuwal</p>
+      </footer>
+    </>
+  )
+}
+export default Footer
+
+
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import Button from './Button'
+const Main = () => {
+  return (
+    <>
+    <div className='container'>
+    <div className='p-5 text-center bg-light-dark rounded'>
+      <h1 className='text-light'>Nepse Prediction Portal</h1>
+      <p className='text-light lead'>This stock prediction apllication is blah blah</p>
+      <Button text = 'Login' class = "btn-outline-info"/>
+    </div>
+
+    </div>
+    </>
+  )
+
+}
+export default Main
+
+
+
